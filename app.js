efdfbbc069f57ace2442d921979f2956239e8a1a
Aurimas <aurimas@darguzis.com>
2017-08-18 16:12:39 +0000

    base structure ready

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3030
+const serveIndex = require('serve-index')
+
+app.use(express.static('./public/'))
+app.use(serveIndex('/public/', {
+    icons: true
+}))
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port)
+    console.log('env = ' + app.get('env') +
+                '\n__dirname = ' + __dirname +
+                '\nprocess.cwd = ' + process.cwd())
+
+})
